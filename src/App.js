7de1753cc76896ea229c3489c318116b454f0ca1
Masum Billah <masum.monpura@gmail.com>
2022-12-13 01:05:21 +0600

    add contact media

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Header from "./Components/Header/Header";
@@ -6,17 +5,72 @@ import About from "./Components/About/About";
 import Work from "./Components/Work/Work";
 import Contact from "./Components/Contact/Contact";
 import { Toaster } from "react-hot-toast";
+import line from "../src/images/line.png";
+import { FaFacebook, FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="body">
+    <div className="body overflow-hidden">
       <Navbar></Navbar>
-      <div className="md:px-16">
+      <div className="md:px-16 relative">
         <Header></Header>
         <About></About>
         <Work></Work>
         <Contact></Contact>
         <Toaster></Toaster>
+
+        <div className=" md:hidden lg:block">
+          <div>
+            <a
+              className="text-[#64FFDA] vertical-r"
+              href="mailto:masum.monpura@gmail.com"
+            >
+              masum.monpura@gmail.com
+            </a>
+            <img className="line-r" src={line} alt="" />
+          </div>
+          <div>
+            <ul className="vertical-l">
+              <li className="text-[#CCD6F6] hover:text-[#64FFDA] mb-5 text-2xl">
+                <a
+                  href="https://github.com/masum-71"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub></FaGithub>
+                </a>
+              </li>
+              <li className="text-[#CCD6F6] hover:text-[#64FFDA] mb-5 text-2xl">
+                <a
+                  href="https://www.linkedin.com/in/masumbillah71/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaLinkedinIn></FaLinkedinIn>
+                </a>
+              </li>
+              <li className="text-[#CCD6F6] hover:text-[#64FFDA] mb-5 text-2xl">
+                <a
+                  href="https://www.facebook.com/Masum.1971/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaFacebook></FaFacebook>
+                </a>
+              </li>
+              <li className="text-[#CCD6F6] hover:text-[#64FFDA] mb-5 text-2xl">
+                <a
+                  href="https://twitter.com/masumbillah_71"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaTwitter></FaTwitter>
+                </a>
+              </li>
+            </ul>
+            <img className="line-l" src={line} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Header.css";
 import resume from "../../resume/MasumBillah.pdf";
 
 const Header = () => {
@@ -6,10 +7,35 @@ const Header = () => {
     <div id="home" className="font-mono py-20">
       <p className="text-[#64FFDA] mb-5">Hi, My Name is</p>
       <p className="lg:text-7xl md:text-5xl sm:text-3xl text-2xl text-[#CCD6F6] mb-5 font-serif">
-        Masum Billah.
+        {/* Masum Billah. */}
+        <span className="hover:animate-ping">M</span>
+        <span className="hover:animate-ping">a</span>
+        <span className="hover:animate-ping">s</span>
+        <span className="hover:animate-ping">u</span>
+        <span className="hover:animate-ping">m</span>
       </p>
       <p className="lg:text-7xl md:text-5xl sm:text-3xl text-2xl text-[#8892B0] mb-5 font-serif">
-        I am a web developer
+        {/* I am a web developer */}
+        <span className="hover:animate-ping">I</span>
+        <span className="hover:animate-ping"> </span>
+        <span className="hover:animate-ping">a</span>
+        <span className="hover:animate-ping">m</span>
+        <span className="hover:animate-ping"> </span>
+        <span className="hover:animate-ping">a</span>
+        <span className="hover:animate-ping"> </span>
+        <span className="hover:animate-ping">w</span>
+        <span className="hover:animate-ping">e</span>
+        <span className="hover:animate-ping">b</span>
+        <span className="hover:animate-ping"> </span>
+        <span className="hover:animate-ping">d</span>
+        <span className="hover:animate-ping">e</span>
+        <span className="hover:animate-ping">v</span>
+        <span className="hover:animate-ping">e</span>
+        <span className="hover:animate-ping">l</span>
+        <span className="hover:animate-ping">o</span>
+        <span className="hover:animate-ping">p</span>
+        <span className="hover:animate-ping">e</span>
+        <span className="hover:animate-ping">r</span>
       </p>
       <p className="md:w-1/2 text-[#8892B0] mb-5">
         I am a MERN stack web developer. I start learning web development since
